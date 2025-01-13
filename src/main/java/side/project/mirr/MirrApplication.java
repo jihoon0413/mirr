d0f62c2efbd69806e6a24933b6a85af2f406a6e3
unknown <wlgnstls04132@gmail.com>
2025-01-13 19:54:12 +0900

    first commit

---
+++ src/main/java/side/project/mirr/MirrApplication.java
@@ -0,0 +1,13 @@
+package side.project.mirr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MirrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MirrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/side/project/mirr/MirrApplicationTests.java
@@ -0,0 +1,13 @@
+package side.project.mirr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MirrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
