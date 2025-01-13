1a6fd2f20f79ae353ee85017f9089090f782bc53
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2025-01-13 21:56:44 +0900

    테스트

--- src/main/java/side/project/mirr/controller/MainController.java
+++ src/main/java/side/project/mirr/controller/MainController.java
@@ -1,13 +1,22 @@
 package side.project.mirr.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import side.project.mirr.service.PlayerService;
 
+@Slf4j
 @Controller
+@RequiredArgsConstructor
 public class MainController {
+
+    private final PlayerService playerService;
 
     @GetMapping("/")
     public String home() {
+        playerService.savePlayer();
         return "index";
     }
 
