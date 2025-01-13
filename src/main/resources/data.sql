9e56b150269e1463d561a0a27c045cf38bcb9c4c
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2025-01-13 21:57:18 +0900

    데이터추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into player (id, name, pos, back_num) values (1, '신지훈', 'CM', 43);
+insert into player (id, name, pos, back_num) values (2, '김민재', 'CM', 10);
+insert into player (id, name, pos, back_num) values (3, '노형남', 'CB', 3);
+
+
+--INSERT INTO `player` VALUES
+--(1,'신지훈','CM',43);
